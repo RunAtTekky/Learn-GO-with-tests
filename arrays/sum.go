60a0b2a1407c14299b5a520f4a51b155e6865ff8
RunAtTekky <varunrawat343@gmail.com>
2025-08-21 16:55:37 +0530

    Test Sum of Tails

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,3 +6,20 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbers_to_sum ...[]int) []int {
+	n := len(numbers_to_sum)
+	res := make([]int, n)
+
+	for i, numbers := range numbers_to_sum {
+		res[i] = Sum(numbers)
+	}
+	return res
+}
+
+func SumTails(tails_to_sum ...[]int) (res []int) {
+	for _, numbers := range tails_to_sum {
+		res = append(res, Sum(numbers[1:]))
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -13,6 +14,37 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d but want %d given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Test sum of all slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{3, 9})
+		want := []int{6, 12}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d but want %d", got, want)
+		}
+	})
+
+	t.Run("Test sum of 3 slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{3, 9}, []int{1})
+		want := []int{6, 12, 1}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d but want %d", got, want)
+		}
+	})
+}
+
+func TestSumTails(t *testing.T) {
+	t.Run("Test sum of tails of slices", func(t *testing.T) {
+		got := SumTails([]int{1, 2, 3, 4}, []int{1, 2, 3}, []int{1})
+		want := []int{9, 5, 0}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d but want %d", got, want)
 		}
 	})
 }
