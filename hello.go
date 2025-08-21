d1eab2462aeee71b34fe8be2365c00ad8ae3ae54
RunAtTekky <varunrawat343@gmail.com>
2025-08-21 15:58:33 +0530

    Spanish and French greeting with refactor

--- hello.go
+++ hello.go
@@ -1,14 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const english_prefix = "Hello"
+const (
+	spanish = "spanish"
+	french  = "french"
 
-func Hello(name string) string {
+	english_prefix = "Hello"
+	spanish_prefix = "Hola"
+	french_prefix  = "Bonjour"
+)
+
+func get_greeting(language string) (greeting string) {
+	switch language {
+	case spanish:
+		greeting = spanish_prefix
+	case french:
+		greeting = french_prefix
+	default:
+		greeting = english_prefix
+	}
+
+	return
+}
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("%s, %s", english_prefix, name)
+
+	return fmt.Sprintf("%s, %s", get_greeting(language), name)
+
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("RunAt")
+		got := Hello("RunAt", "")
 		want := "Hello, RunAt"
 
 		if got != want {
@@ -13,11 +13,30 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("saying 'Hello, World' when supplied empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
+	t.Run("saying 'Hola, World' when supplied Spanish", func(t *testing.T) {
+		got := Hello("", "spanish")
+		want := "Hola, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("saying 'Bonjour, World' when supplied French", func(t *testing.T) {
+		got := Hello("", "french")
+		want := "Bonjour, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
 }
