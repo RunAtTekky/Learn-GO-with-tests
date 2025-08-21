844518963060220ed901cf9c014572a1dd440593
RunAtTekky <varunrawat343@gmail.com>
2025-08-21 16:40:20 +0530

    Refactor Sum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,14 +1,18 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Sum of slice", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d but want %d given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d but want %d given %v", got, want, numbers)
+		}
+	})
 }
