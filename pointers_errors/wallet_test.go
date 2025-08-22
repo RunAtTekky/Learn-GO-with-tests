258ed06970e063f8cbc408d84497cf326dd86799
RunAtTekky <varunrawat343@gmail.com>
2025-08-22 17:44:38 +0530

    [Pointers & Errors] Implement Withdraw and refactor
    Refactor with assertBalance function

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -3,13 +3,33 @@ package pointerserrors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s but want %s", got, want)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.balance
+		if got != want {
+			t.Errorf("got %s but want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit Test", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw Test", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw more than available Test", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(20))
+
+		assertBalance(t, wallet, Bitcoin(-10))
+	})
 }
