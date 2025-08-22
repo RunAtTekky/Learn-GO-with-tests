cd9c965d3ec0e2fccb12cab09cf2605d89056b4a
RunAtTekky <varunrawat343@gmail.com>
2025-08-22 20:31:19 +0530

    refactor: Custom Dictionary and error

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,16 @@
 package maps
 
-func Search(dictionary map[string]string, key string) (val string) {
-	return dictionary[key]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNoEntryFound = errors.New("No entry found for your word")
+
+func (d Dictionary) Search(key string) (val string, err error) {
+	definition, ok := d[key]
+
+	if !ok {
+		return "", ErrNoEntryFound
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,38 @@ package maps
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	dictionary := map[string]string{"naruto": "A shonen anime."}
+	dictionary := Dictionary{"naruto": "A shonen anime."}
 
-	got := Search(dictionary, "naruto")
-	want := "A shonen anime."
+	t.Run("Known Test", func(t *testing.T) {
 
+		got, _ := dictionary.Search("naruto")
+		want := "A shonen anime."
+
+		assertString(t, got, want)
+	})
+
+	t.Run("Unknown Test", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := ErrNoEntryFound
+
+		if err == nil {
+			t.Fatal("Wanted an error but got none")
+		}
+
+		assertError(t, err, want)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q but want %q given, %q", got, want, dictionary)
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
 	}
 }
