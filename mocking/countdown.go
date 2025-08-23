fbbd4901a7baa0ed80353eaaf86fad4af8c221ac
RunAtTekky <varunrawat343@gmail.com>
2025-08-23 23:59:33 +0530

    [Mocking] add default and spy sleeper
    In testing our tests were taking 3 seconds coz of sleep calls
    We mock these calls using our spysleeper in tests and realsleeper in
    main function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,12 +4,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(writer io.Writer) {
-	fmt.Fprintf(writer, "3")
+const (
+	COUNT_DOWN_START = 3
+	FINAL_WORD       = "GO!"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := COUNT_DOWN_START; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(writer, FINAL_WORD)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -7,13 +7,21 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+GO!`
 
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, got %d", spySleeper.Calls)
 	}
 }
