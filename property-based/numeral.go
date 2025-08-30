6491d2c2e1c5dc0633f50c1be244d5d3a084a9d4
RunAtTekky <varunrawat343@gmail.com>
2025-08-30 11:57:55 +0530

    [Property-based] All symbols are supported now and we have moved tests as package variables
    add: C D M symbols and values
    
    add more test cases with C D and M
    
    move tests outside to make it package variable, add RomanToArabic

--- property-based/numeral.go
+++ property-based/numeral.go
@@ -8,6 +8,14 @@ type RomanNumeral struct {
 }
 
 var allRomanNumerals = []RomanNumeral{
+	{value: 1000, symbol: "M"},
+	{value: 900, symbol: "CM"},
+	{value: 500, symbol: "D"},
+	{value: 400, symbol: "CD"},
+	{value: 100, symbol: "C"},
+	{value: 90, symbol: "XC"},
+	{value: 50, symbol: "L"},
+	{value: 40, symbol: "XL"},
 	{value: 10, symbol: "X"},
 	{value: 9, symbol: "IX"},
 	{value: 5, symbol: "V"},
@@ -15,13 +23,17 @@ var allRomanNumerals = []RomanNumeral{
 	{value: 1, symbol: "I"},
 }
 
-func ConvertToRoman(num int) string {
+func ConvertToArabic(roman string) int {
+	return 0
+}
+
+func ConvertToRoman(arabic int) string {
 	var roman strings.Builder
 
 	for _, numeral := range allRomanNumerals {
-		for num >= numeral.value {
+		for arabic >= numeral.value {
 			roman.WriteString(numeral.symbol)
-			num -= numeral.value
+			arabic -= numeral.value
 		}
 	}
 
--- property-based/numeral_test.go
+++ property-based/numeral_test.go
@@ -1,34 +1,63 @@
 package propertybased
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRomanNumeral(t *testing.T) {
-	tests := []struct {
-		name string
-		want string
-		num  int
+var (
+	tests = []struct {
+		roman  string
+		arabic int
 	}{
-		{name: "Convert 1 to I", want: "I", num: 1},
-		{name: "Convert 2 to II", want: "II", num: 2},
-		{name: "Convert 3 to III", want: "III", num: 3},
-		{name: "Convert 4 to IV", want: "IV", num: 4},
-		{name: "Convert 5 to V", want: "V", num: 5},
-		{name: "Convert 6 to VI", want: "VI", num: 6},
-		{name: "Convert 7 to VII", want: "VII", num: 7},
-		{name: "Convert 8 to VIII", want: "VIII", num: 8},
-		{name: "Convert 9 to IX", want: "IX", num: 9},
-		{name: "Convert 11 to XI", want: "XI", num: 11},
-		{name: "Convert 12 to XII", want: "XII", num: 12},
-		{name: "Convert 22 to XXII", want: "XXII", num: 22},
-		{name: "Convert 35 to XXXV", want: "XXXV", num: 35},
-		{name: "Convert 39 to XXXIX", want: "XXXIX", num: 39},
+		{roman: "I", arabic: 1},
+		{roman: "II", arabic: 2},
+		{roman: "III", arabic: 3},
+		{roman: "IV", arabic: 4},
+		{roman: "V", arabic: 5},
+		{roman: "VI", arabic: 6},
+		{roman: "VII", arabic: 7},
+		{roman: "VIII", arabic: 8},
+		{roman: "IX", arabic: 9},
+		{roman: "XI", arabic: 11},
+		{roman: "XII", arabic: 12},
+		{roman: "XXII", arabic: 22},
+		{roman: "XXXV", arabic: 35},
+		{roman: "XXXIX", arabic: 39},
+		{roman: "XL", arabic: 40},
+		{roman: "XLVII", arabic: 47},
+		{roman: "XLIX", arabic: 49},
+		{roman: "L", arabic: 50},
+		{roman: "M", arabic: 1000},
+		{roman: "MCMLXXXIV", arabic: 1984},
+		{roman: "MMMCMXCIX", arabic: 3999},
+		{roman: "MCMLXXXIV", arabic: 1984},
+		{roman: "MMXIV", arabic: 2014},
+		{roman: "MVI", arabic: 1006},
+		{roman: "DCCXCVIII", arabic: 798},
 	}
+)
 
+func TestArabicToRoman(t *testing.T) {
 	for _, tt := range tests {
-		got := ConvertToRoman(tt.num)
+		t.Run(fmt.Sprintf("Convert %d to %q", tt.arabic, tt.roman), func(t *testing.T) {
+			got := ConvertToRoman(tt.arabic)
 
-		if got != tt.want {
-			t.Errorf("got %q but want %q", got, tt.want)
-		}
+			if got != tt.roman {
+				t.Errorf("got %q but roman %q", got, tt.roman)
+			}
+		})
+	}
+}
+
+func TestRomanToArabic(t *testing.T) {
+	for _, tt := range tests[:1] {
+		t.Run(fmt.Sprintf("Convert %q to %d", tt.roman, tt.arabic), func(t *testing.T) {
+			arabic := ConvertToArabic(tt.roman)
+
+			if arabic != tt.arabic {
+				t.Errorf("got %d but want %d", arabic, tt.arabic)
+			}
+		})
 	}
 }
