fb5fee4e83fbc09f635f9ec62536fc4e2bafa634
RunAtTekky <varunrawat343@gmail.com>
2025-12-14 00:10:21 +0530

    Refactor: use fs.FS instead of fstest.MapFS

--- blogposts.go
+++ blogposts.go
@@ -2,13 +2,12 @@ package blogposts
 
 import (
 	"io/fs"
-	"testing/fstest"
 )
 
 type Post struct {
 }
 
-func NewPostsFromFS(filesystem fstest.MapFS) []Post {
+func NewPostsFromFS(filesystem fs.FS) []Post {
 	dir, _ := fs.ReadDir(filesystem, ".")
 
 	var posts []Post
