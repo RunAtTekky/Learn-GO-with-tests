37913db614d831f7396b029965f0e7688567f682
RunAtTekky <varunrawat343@gmail.com>
2025-12-14 16:26:24 +0530

    [templating] test: Rendering test is failing as expected

---
+++ blogrender/blogrender.go
@@ -0,0 +1,7 @@
+package blogrender
+
+import "io"
+
+func Render(file io.Writer, post Post) error {
+	return nil
+}
---
+++ blogrender/blogrender_test.go
@@ -0,0 +1,35 @@
+package blogrender_test
+
+import (
+	"bytes"
+	blogrender "go_with_test/blogrender"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrender.Post{
+			Title:       "Hello World",
+			Description: "Say Hello World",
+			Tags:        []string{"go", "tdd"},
+			Body:        "This is the body",
+		}
+	)
+
+	t.Run("coverts single Post to HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		err := blogrender.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `This is the body`
+
+		if got != want {
+			t.Errorf("got %s but want %s", got, want)
+		}
+	})
+}
