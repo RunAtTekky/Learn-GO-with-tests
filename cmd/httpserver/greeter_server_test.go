5fc5bac0b7a9c97eded918cd9f062a3365c75814
RunAtTekky <varunrawat343@gmail.com>
2025-12-20 00:12:40 +0530

    add: driver.go

--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -3,9 +3,11 @@ package main_test
 import (
 	"testing"
 
+	go_specs_greet "github.com/runattekky/go-specs-greet"
 	"github.com/runattekky/go-specs-greet/specifications"
 )
 
 func TestGreeterServer(t *testing.T) {
-	specifications.GreetSpecification(t, nil)
+	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
+	specifications.GreetSpecification(t, driver)
 }
