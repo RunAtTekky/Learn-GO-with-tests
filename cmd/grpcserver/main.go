d975c41bcc58666cb056c5c44975b95f2bb30f13
RunAtTekky <varunrawat343@gmail.com>
2025-12-21 00:29:13 +0530

    write grpcserver/main.go

--- cmd/grpcserver/main.go
+++ cmd/grpcserver/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/runattekky/go-specs-greet/adapters/grpcserver"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Implement me")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type GreetServer struct {
+	grpcserver.UnimplementedGreeterServer
+}
+
+func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
+	return &grpcserver.GreetReply{Message: "fix me"}, nil
 }
