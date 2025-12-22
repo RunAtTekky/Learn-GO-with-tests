6dcad47aeb2f2d8385b17887374cd846dcc4dc61
RunAtTekky <varunrawat343@gmail.com>
2025-12-22 20:42:51 +0530

    add: Curse to httpserver

--- adapters/httpserver/handler.go
+++ adapters/httpserver/handler.go
@@ -7,7 +7,22 @@ import (
 	"github.com/runattekky/go-specs-greet/domain/interactions"
 )
 
-func Handler(writer http.ResponseWriter, request *http.Request) {
-	name := request.URL.Query().Get("name")
-	fmt.Fprint(writer, interactions.Greet(name))
+const (
+	greetPath = "/greet"
+	cursePath = "/curse"
+)
+
+func NewHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(greetPath, replyWith(interactions.Greet))
+	mux.HandleFunc(cursePath, replyWith(interactions.Curse))
+	return mux
+}
+
+func replyWith(f func(name string) string) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		name := request.URL.Query().Get("name")
+		fmt.Fprint(writer, f(name))
+	}
+
 }
