5aafa3767f8770096f0dd34cfabb59c5434a7940
RunAtTekky <varunrawat343@gmail.com>
2025-12-23 23:49:13 +0530

    green: Correct winCall stored

--- server/server.go
+++ server/server.go
@@ -18,23 +18,29 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case http.MethodPost:
-		p.processWin(writer)
+		p.processWin(writer, request)
 	case http.MethodGet:
 		p.showScore(writer, request)
 	}
 }
 
-func (p *PlayerServer) processWin(writer http.ResponseWriter) {
+func (p *PlayerServer) processWin(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(http.StatusAccepted)
-	p.Store.RecordWin("RunAt")
+	player := getPlayer(request.URL.Path)
+	p.Store.RecordWin(player)
 }
 
 func (p *PlayerServer) showScore(writer http.ResponseWriter, request *http.Request) {
-	player := strings.TrimPrefix(request.URL.Path, "/players/")
+	player := getPlayer(request.URL.Path)
 	score := p.Store.GetPlayerScore(player)
 	if score == 0 {
 		writer.WriteHeader(http.StatusNotFound)
 	}
 	fmt.Fprint(writer, score)
 
+}
+
+func getPlayer(path string) string {
+	player := strings.TrimPrefix(path, "/players/")
+	return player
 }
