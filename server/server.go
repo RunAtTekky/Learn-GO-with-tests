bfaa538e974ac886cead5e6e62acd38bc095648b
RunAtTekky <varunrawat343@gmail.com>
2025-12-23 23:14:46 +0530

    refactor

--- server/server.go
+++ server/server.go
@@ -15,14 +15,24 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	if request.Method == http.MethodPost {
-		writer.WriteHeader(http.StatusAccepted)
-		return
+	switch request.Method {
+	case http.MethodPost:
+		p.processWin(writer)
+	case http.MethodGet:
+		p.showScore(writer, request)
 	}
+}
+
+func (p *PlayerServer) processWin(writer http.ResponseWriter) {
+	writer.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) showScore(writer http.ResponseWriter, request *http.Request) {
 	player := strings.TrimPrefix(request.URL.Path, "/players/")
 	score := p.Store.GetPlayerScore(player)
 	if score == 0 {
 		writer.WriteHeader(http.StatusNotFound)
 	}
 	fmt.Fprint(writer, score)
+
 }
