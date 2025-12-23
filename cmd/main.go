96d035a55b5df5bff7226cd3856a2b011baeed38
RunAtTekky <varunrawat343@gmail.com>
2025-12-23 20:47:23 +0530

    application now builds and runs but only gives out 123 as answer

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,13 @@ import (
 	"github.com/runattekky/go-app/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	svr := &server.PlayerServer{}
+	svr := &server.PlayerServer{Store: &InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", svr))
 }
