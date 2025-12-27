9924090b2b6a3260ff7feb3f78f965722286d852
RunAtTekky <varunrawat343@gmail.com>
2025-12-27 19:42:55 +0530

    record win function is working

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -39,6 +39,21 @@ func TestFileSystemStore(t *testing.T) {
 		want := 10
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+		{"Name": "RunAt", "Wins": 10},
+		{"Name": "Ronaldo", "Wins": 5}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("RunAt")
+
+		got := store.GetPlayerScore("RunAt")
+		want := 11
+		assertScoreEquals(t, got, want)
+	})
 }
 
 func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
