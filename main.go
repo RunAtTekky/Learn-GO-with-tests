a6d83ed5670df561ea6b06378c95a8107ea1ba4d
RunAtTekky <varunrawat343@gmail.com>
2025-12-27 20:07:55 +0530

    database persists between restarts

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	svr := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":5000", svr))
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("Problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	svr := NewPlayerServer(store)
+	if err := http.ListenAndServe(":5000", svr); err != nil {
+		log.Fatalf("Could not listen on PORT :5000 %v", err)
+	}
 }
