5e4f0c748f75f1572ad821167ccef00f6b8d3d5b
RunAtTekky <varunrawat343@gmail.com>
2025-12-27 19:53:45 +0530

    use League.Find()

--- file_system_store.go
+++ file_system_store.go
@@ -10,29 +10,25 @@ type FileSystemPlayerStore struct {
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	score := 0
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			score = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+
+	if player != nil {
+		return player.Wins
 	}
-	return score
+
+	return 0
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
-	for i, player := range league {
-		if player.Name == name {
-			league[i].Wins++
-		}
-	}
+	player := league.Find(name)
+	player.Wins++
 
 	f.database.Seek(0, io.SeekStart)
 	json.NewEncoder(f.database).Encode(league)
 }
 
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(f.database)
 	return league
