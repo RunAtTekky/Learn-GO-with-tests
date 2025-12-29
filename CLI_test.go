a037ec5950f848f14efd5781ca960d1a44a29fcb
RunAtTekky <varunrawat343@gmail.com>
2025-12-29 14:20:53 +0530

    green: CLI test

--- CLI_test.go
+++ CLI_test.go
@@ -1,13 +1,24 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("RunAt wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
 	if len(playerStore.winCalls) != 1 {
 		t.Fatal("Expected a win call but did not get any")
+	}
+
+	got := playerStore.winCalls[0]
+	want := "RunAt"
+
+	if got != want {
+		t.Errorf("did not record correct winner, got %s but want %s", got, want)
 	}
 }
