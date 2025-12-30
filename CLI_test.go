29896de9204277c738ec40e5af8311715bda8909
RunAtTekky <varunrawat343@gmail.com>
2025-12-30 20:07:16 +0530

    red: schedules printing of blind values

--- CLI_test.go
+++ CLI_test.go
@@ -1,34 +1,57 @@
-package poker
+package poker_test
 
 import (
 	"strings"
 	"testing"
+	"time"
+
+	poker "github.com/runattekky/go-app"
 )
+
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
+}
 
 func TestCLI(t *testing.T) {
 	t.Run("record RunAt win from user input", func(t *testing.T) {
 		in := strings.NewReader("RunAt wins\n")
-		playerStore := &StubPlayerStore{}
-		cli := NewCLI(playerStore, in)
+		playerStore := &poker.StubPlayerStore{}
+		dummySpyAlerter := &SpyBlindAlerter{}
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
-		if len(playerStore.winCalls) != 1 {
-			t.Fatal("Expected a win call but did not get any")
-		}
-
-		AssertPlayerWin(t, playerStore, "RunAt")
+		poker.AssertPlayerWin(t, playerStore, "RunAt")
 	})
 
 	t.Run("record Ronaldo win from user input", func(t *testing.T) {
 		in := strings.NewReader("Ronaldo wins\n")
-		playerStore := &StubPlayerStore{}
-		cli := NewCLI(playerStore, in)
+		playerStore := &poker.StubPlayerStore{}
+		dummySpyAlerter := &SpyBlindAlerter{}
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
 		cli.PlayPoker()
 
-		if len(playerStore.winCalls) != 1 {
-			t.Fatal("Expected a win call but did not get any")
-		}
+		poker.AssertPlayerWin(t, playerStore, "Ronaldo")
+	})
 
-		AssertPlayerWin(t, playerStore, "Ronaldo")
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Mbappe wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
 	})
 }
